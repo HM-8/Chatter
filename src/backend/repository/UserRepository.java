14ec68de11277f5cd8f219d48a5a0604eb1c4a81
Hiwot Medhin <hiwotmedhings@gmail.com>
2021-03-06 01:44:55 +0300

    Fix minor detail

--- src/backend/repository/UserRepository.java
+++ src/backend/repository/UserRepository.java
@@ -2,18 +2,25 @@ package backend.repository;
 
 import backend.DatabaseConnectionHandler;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
 
 public class UserRepository {
-    private static final Connection connection = DatabaseConnectionHandler.getHandler().getConnection();
+    private static Statement statement;
+
+    static {
+        try {
+            statement = DatabaseConnectionHandler.getHandler().getConnection().createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public ResultSet selectSingleUser(String[] user) {
         String query = MessageFormat.format("SELECT count(1) FROM users WHERE user_name = ''{0}'' AND password = ''{1}''", user);
         try {
-            Statement statement = connection.createStatement();
             ResultSet queryResult = statement.executeQuery(query);
             return queryResult;
         } catch (Exception e) {
@@ -27,7 +34,6 @@ public class UserRepository {
         String query = MessageFormat.format("INSERT INTO users (first_name, last_name, user_name, password) Values (''{0}'', ''{1}'', ''{2}'', ''{3}'')", user);
 
         try {
-            Statement statement = connection.createStatement();
             int queryResult = statement.executeUpdate(query);
 
             if(queryResult == 1)
@@ -45,7 +51,6 @@ public class UserRepository {
         String query = MessageFormat.format("SELECT count(1) FROM users WHERE user_name = ''{0}''", userName);
 
         try {
-            Statement statement = connection.createStatement();
             ResultSet queryResult = statement.executeQuery(query);
 
             if(queryResult.getInt(1) == 1)
