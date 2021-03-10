89a0a303195d120c8fa677e220d8c6b01b633137
Ezana Fitsum <mruniverse1x@gmail.com>
2021-03-10 17:11:07 +0300

    created a new chat.fxml and updated the chatController

--- src/backend/controller/ChatController.java
+++ src/backend/controller/ChatController.java
@@ -2,8 +2,9 @@ package backend.controller;
 
 import backend.Client;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -14,16 +15,34 @@ public class ChatController {
     private static DataOutputStream out=Client.getOutgoingStream();
 
     @FXML
-    private static TextField chatTextField;
+    private static TextField chat_text_field;
     @FXML
-    private static TextArea chatTextArea;
+    private static Label username_label;
+    @FXML
+    private static ListView person_info;
+    @FXML
+    private static ListView chat_list;
+    @FXML
+    private static Label username_text;
+    @FXML
+    private static ScrollPane chat_display_panel;
 
+    private static Label msg=null;
+    private static Label sent=null;
     public static void main(String[] args) throws IOException {
+
         String text=in.readUTF();
-        chatTextArea.setText(chatTextArea.getText()+ "\n Server:" + text);
+        msg.setText(text);
+        msg.setAlignment(Pos.BASELINE_LEFT);
+        chat_display_panel.setContent(msg);
+//        chatTextArea.setText(chatTextArea.getText()+ "\n Server:" + text);
     }
     public void TextHandler() throws IOException {
-        out.writeUTF(String.valueOf(chatTextField));
+        out.writeUTF(String.valueOf(chat_text_field));
+        String sent_msg=String.valueOf(chat_text_field);
+        sent.setText(sent_msg);
+        sent.setAlignment(Pos.BASELINE_RIGHT);
+        chat_display_panel.setContent(msg);
     }
 
 }
