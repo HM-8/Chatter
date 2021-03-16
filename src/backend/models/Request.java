131ba26abbcaf68ab43e22e62d3f0c56afecca7a
Ezra Ashenafi <eazash22@gmail.com>
2021-03-16 20:22:05 +0300

    rewrite Request to make data property an array list

--- src/backend/models/Request.java
+++ src/backend/models/Request.java
@@ -1,15 +1,20 @@
 package backend.models;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Request extends JSONizable {
     public String route;
-    public Object data;
+    public ArrayList<String> data = new ArrayList<>();
 
     public Request() {
     }
 
-    public Request(String route, Object data) {
+    public Request(String route, String[] data) {
         this.route = route;
-        this.data = data;
+        this.data.addAll(Arrays.asList(data));
     }
 }
