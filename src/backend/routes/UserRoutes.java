a5901731362dda7dcaf511a0d3ed4834c039f454
Ezra Ashenafi <eazash22@gmail.com>
2021-03-16 20:20:33 +0300

    rewrite to set chat title appropriately

--- src/backend/routes/UserRoutes.java
+++ src/backend/routes/UserRoutes.java
@@ -34,12 +34,12 @@ public class UserRoutes {
         try {
             ResultSet qr = statement.executeQuery(query);
             qr.next();
-            User user = new User(qr.getInt(1), qr.getString(2), qr.getString(3), qr.getString(4)    );
+            User user = new User(qr.getInt(1), qr.getString(2), qr.getString(3), qr.getString(4));
             return user;
         } catch (SQLException e) {
-                System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
             System.out.println(e.getSQLState());
-            if(e.getSQLState() == "S1000") {
+            if (e.getSQLState() == "S1000") {
                 return new ErrorMessage("Username or password Incorrect");
             }
             return null;
@@ -60,7 +60,7 @@ public class UserRoutes {
                 return new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
             }
         } catch (SQLException e) {
-            if(e.getSQLState() == "23000"){
+            if (e.getSQLState() == "23000") {
                 return new ErrorMessage("Username already exists, try again");
             }
         }
@@ -69,12 +69,12 @@ public class UserRoutes {
 
     public static Chat[] getChats(ArrayList data) {
         String user_id = String.valueOf(data.get(0));
-        String query = String.format("SELECT c.id as chat_id, c.type as chat_type, u.id as user_id, u.first_name , u.last_name, u.user_name from chats c JOIN users_chats cv1 on cv1.user_id = %s and c.id = cv1.chat_id JOIN users_chats cv2 on cv2.chat_id = cv1.chat_id JOIN users u on cv2.user_id = u.id;",user_id);
+        String query = String.format("SELECT c.id as chat_id, c.type as chat_type, u.id as user_id, u.first_name , u.last_name, u.user_name from chats c JOIN users_chats cv1 on cv1.user_id = %s and c.id = cv1.chat_id JOIN users_chats cv2 on cv2.chat_id = cv1.chat_id JOIN users u on cv2.user_id = u.id;", user_id);
         try {
             ResultSet rs = statement.executeQuery(query);
             ArrayList<Chat> chatArrayList = new ArrayList<>();
             Chat newChat;
-            while(rs.next()){
+            while (rs.next()) {
                 String chatType = rs.getString("chat_type");
                 newChat = new Chat(rs.getInt("chat_id"));
                 //Create a newMember
@@ -88,17 +88,22 @@ public class UserRoutes {
                 if (chatArrayList.contains(newChat)) {
                     Chat existingChat = chatArrayList.get(chatArrayList.indexOf(newChat));
                     existingChat.addMember(newMember);
-                }else{
+                    if(chatType.equals("user") && !(String.valueOf(newMember.id).equals(user_id))){
+                        existingChat.setTitle(newMember.username);
+                    }
+                } else {
                     //if the chat is not in the list add it to the list and add a member to it
                     newChat.addMember(newMember);
-                    if(chatType.equals("user")){
+                    if (chatType.equals("user")) {
                         //if the chat is of type 'user', set its title to the user's username
-                        newChat.setTitle(newChat.getMembers().stream().filter(user -> user.id!=Integer.parseInt(user_id)).findFirst().get().username);
+                        if (newMember.id != Integer.parseInt(user_id)) {
+                            newChat.setTitle(newMember.username);
+                        }
                     }
                     chatArrayList.add(newChat);
                 }
             }
-            Chat [] chatsArray = new Chat[chatArrayList.size()];
+            Chat[] chatsArray = new Chat[chatArrayList.size()];
             return chatArrayList.toArray(chatsArray);
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
