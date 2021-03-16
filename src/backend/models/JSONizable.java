dd093e5a26523cde239920c8965ec660c23edc05
Ezra Ashenafi <eazash22@gmail.com>
2021-03-16 10:02:53 +0300

    add method to parse JSON of array

--- src/backend/models/JSONizable.java
+++ src/backend/models/JSONizable.java
@@ -1,10 +1,15 @@
 package backend.models;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
+@JsonSubTypes.Type(value = Chat.class, name = "chat")
 public class JSONizable {
     private static ObjectMapper mapper = new ObjectMapper();
 
@@ -16,5 +21,8 @@ public class JSONizable {
     }
     public static JSONizable fromJSON(String JSON) throws JsonProcessingException {
         return mapper.readValue(JSON, JSONizable.class);
+    }
+    public static JSONizable[] fromJSONArray(String JSONarray, Class c) throws JsonProcessingException {
+        return (JSONizable[]) mapper.readValue(JSONarray, c);
     }
 }
