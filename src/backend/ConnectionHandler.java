adb72bb4cbcadb20f11d21983842ad3578dd2fb0
Ezra Ashenafi <eazash22@gmail.com>
2021-03-16 10:19:15 +0300

    rewrite thread to close on socket errors

--- src/backend/ConnectionHandler.java
+++ src/backend/ConnectionHandler.java
@@ -1,9 +1,6 @@
 package backend;
 
-import backend.models.ErrorMessage;
-import backend.models.JSONizable;
-import backend.models.Message;
-import backend.models.Request;
+import backend.models.*;
 import backend.routes.MessageRoutes;
 import backend.routes.UserRoutes;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +9,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 
 public class ConnectionHandler implements Runnable {
@@ -19,6 +17,7 @@ public class ConnectionHandler implements Runnable {
     private DataInputStream incomingStream;
     private DataOutputStream outgoingStream;
     private static ObjectMapper mapper = new ObjectMapper();
+
     public ConnectionHandler(Socket clientSocket) {
         this.socket = clientSocket;
         try {
@@ -32,15 +31,15 @@ public class ConnectionHandler implements Runnable {
     @Override
     public void run() {
         init();
-        while(true){
+        while (true) {
             try {
                 JSONizable parsedInput = JSONizable.fromJSON(incomingStream.readUTF());
-                if (parsedInput instanceof Request){
+                if (parsedInput instanceof Request) {
                     Request request = (Request) parsedInput;
-                    switch (request.route){
-                        case "login":{
+                    switch (request.route) {
+                        case "login": {
                             JSONizable result = UserRoutes.login((ArrayList) request.data);
-                            if(!(result instanceof ErrorMessage) ){
+                            if (!(result instanceof ErrorMessage)) {
                                 Server.addToConnections(this.socket);
                             }
                             this.outgoingStream.writeUTF(result.toJSON());
@@ -48,7 +47,7 @@ public class ConnectionHandler implements Runnable {
                         }
                         case "signup": {
                             JSONizable result = UserRoutes.signup((ArrayList) request.data);
-                            if(!(result instanceof ErrorMessage) ){
+                            if (!(result instanceof ErrorMessage)) {
                                 Server.addToConnections(this.socket);
                             }
                             this.outgoingStream.writeUTF(result.toJSON());
@@ -60,23 +59,37 @@ public class ConnectionHandler implements Runnable {
                             break;
                         }
                         case "myChats": {
-                            ArrayList userChatsArraylist= UserRoutes.getChats((ArrayList)request.data);
+                            Chat[] userChatsArraylist = UserRoutes.getChats((ArrayList) request.data);
                             this.outgoingStream.writeUTF(mapper.writeValueAsString(userChatsArraylist));
                         }
                     }
-                }else if (parsedInput instanceof Message){
+                } else if (parsedInput instanceof Message) {
                     Message message = (Message) parsedInput;
                     System.out.println(message.toJSON());
                     Server.broadcast(message);
                 }
+            } catch (SocketException se) {
+                System.out.println(se.getMessage());
+                closeThread();
+                return;
             } catch (IOException e) {
                 //When client disconnects
                 System.out.println("Client disconnected");
-                e.printStackTrace();
-                break;
+                closeThread();
             }
         }
     }
+
+    private void closeThread() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            Thread.currentThread().interrupt();
+        }
+    }
+
 
     private void init() {
         //initialize the connection
